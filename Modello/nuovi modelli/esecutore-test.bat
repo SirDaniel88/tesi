24468932934530f6eed853febeb74b28be91703d
SirDaniel88 <sir-daniel@hotmail.it>
2018-02-10 16:23:20 +0100

    Modifica test

--- Modello/nuovi modelli/esecutore-test.bat
+++ Modello/nuovi modelli/esecutore-test.bat
@@ -13,5 +13,16 @@ mosel -c "load Hybrid_Efficient_Model_No_Radiant;run 'k= %%A', 'w = %%Q ', 'h = 
 )
 )
 )
+for /L %%M IN (1,1,6) do (
+for /L %%A IN (1,1,37) do (
+mosel -c "load Additive_Model;run 'k = %%A', 'w = "9" ', 'h = %%M'"
+mosel -c "load BCC_Model;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+mosel -c "load BCC_Output_Oriented;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+mosel -c "load CCR_Dual_Model;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+mosel -c "load CCR_Output_Oriented;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+mosel -c "load SBM_Dual_Model;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+mosel -c "load Hybrid_Efficient_Model_No_Radiant;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+)
+)
 echo fine esecuzione
 pause
