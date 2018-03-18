7cd54e63a4aea214b18d01b3048f5e0f59696a69
SirDaniel88 <sir-daniel@hotmail.it>
2018-03-18 22:27:21 +0100

    Esecuzioni test

--- Modello/nuovi modelli/IndiceMalmquist/esecutore-test.bat
+++ Modello/nuovi modelli/IndiceMalmquist/esecutore-test.bat
@@ -1,21 +1,15 @@
 echo off
 echo Inizio esecuzione
 for /L %%Q IN (1,1,7) do (
-for /L %%M IN (1,1,6) do (
+for /L %%M IN (1,1,5) do (
 for /L %%A IN (1,1,37) do (
-mosel -c "load RadialMI;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
-mosel -c "load RadialMI;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
 mosel -c "load RadialMI_st;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
-mosel -c "load RadialMI_ts;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
 )
 )
 )
-for /L %%M IN (1,1,6) do (
+for /L %%M IN (1,1,5) do (
 for /L %%A IN (1,1,37) do (
-mosel -c "load RadialMI;run 'k = %%A', 'w = "9" ', 'h = %%M'"
-mosel -c "load RadialMI;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
-mosel -c "load RadialMI_st;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
-mosel -c "load RadialMI_ts;run 'k = %%A', 'w = %%Q ', 'h = %%M'"
+mosel -c "load RadialMI_st;run 'k = %%A', 'w = "9" ', 'h = %%M'"
 )
 )
 echo fine esecuzione
