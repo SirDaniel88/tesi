372916b7a1a3db9723a82d0d5fdfb5cd08ba72bf
SirDaniel88 <sir-daniel@hotmail.it>
2018-05-06 21:05:38 +0200

    Commenti e Grafici

--- Modello/nuovi modelli/inputTesiOriginali/esecutore-test-9.bat
+++ Modello/nuovi modelli/inputTesiOriginali/esecutore-test-9.bat
@@ -1,13 +1,15 @@
 echo off
 echo Inizio esecuzione
+
 for /L %%A IN (1,1,37) do (
-mosel -c "load Additive_Model;run 'k = %%A', 'w = "9" '"
-mosel -c "load BCC_Model;run 'k= %%A', 'w = "9" '"
-mosel -c "load BCC_Output_Oriented;run 'k= %%A', 'w = "9" '"
-mosel -c "load CCR_Dual_Model;run 'k= %%A', 'w = "9" '"
-mosel -c "load CCR_Output_Oriented;run 'k= %%A', 'w = "9" '"
-mosel -c "load SBM_Dual_Model;run 'k= %%A', 'w = "9" '"
-mosel -c "load Hybrid_Efficient_Model_No_Radiant;run 'k= %%A', 'w = "9" '"
+mosel -c "load Additive_Model;run 'k = %%A', 'w = "7" ', 'h = 4'"
+mosel -c "load BCC_Model;run 'k= %%A', 'w = "7" ', 'h = 4'"
+mosel -c "load BCC_Output_Oriented;run 'k= %%A', 'w = "7" ', 'h = 4'"
+mosel -c "load CCR_Dual_Model;run 'k= %%A', 'w = "7" ', 'h = 4'"
+mosel -c "load CCR_Output_Oriented;run 'k= %%A', 'w = "7" ', 'h = 4'"
+mosel -c "load SBM_Dual_Model;run 'k= %%A', 'w = "7" ', 'h = 4'"
+mosel -c "load Hybrid_Efficient_Model_No_Radiant;run 'k= %%A', 'w = "7" ', 'h = 4'"
 )
+
 echo fine esecuzione
 pause
---
+++ Modello/nuovi modelli/inputTesiOriginali/esecutore-test-SBM.bat
@@ -0,0 +1,16 @@
+echo off
+echo Inizio esecuzione
+for /L %%Q IN (1,1,7) do (
+for /L %%M IN (1,1,6) do (
+for /L %%A IN (1,1,37) do (
+mosel -c "load SBM_Dual_Model;run 'k= %%A', 'w = %%Q ', 'h = %%M'"
+)
+)
+)
+for /L %%M IN (1,1,6) do (
+for /L %%A IN (1,1,37) do (
+mosel -c "load SBM_Dual_Model;run 'k= %%A', 'w = "9" ', 'h = %%M'"
+)
+)
+echo fine esecuzione
+pause
